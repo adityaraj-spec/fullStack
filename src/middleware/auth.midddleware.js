27769434fdc2887dd34c1fd9f3ce7f58420a6d29
Aaditya Raj <aadityajandaha07@gnail.com>
2026-01-28 23:23:31 +0530

    fixes bugs in verifyJWT

--- src/middleware/auth.midddleware.js
+++ src/middleware/auth.midddleware.js
@@ -15,13 +15,13 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        const user = await User.findById(decodedToken?.id).select("-password -refreshToken")
 
-        req.user = user;
-        next()
         if(!user) {
             throw new ApiError(401, "Invalid Access Token")
         }
+        req.user = user;
+        next()
 
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid Access Token")
