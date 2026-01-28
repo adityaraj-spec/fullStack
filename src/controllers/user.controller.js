f2e9bdec50ad3dc3f97718de0af5d7cb88c214f8
Aaditya Raj <aadityajandaha07@gnail.com>
2026-01-28 19:14:04 +0530

    fixes some bugs and  added users in the data base by registering them

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,7 +28,7 @@ const registerUser = asyncHandler( async(req, res) => {
         throw new ApiError (400 , "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if (existedUser) {
@@ -36,7 +36,11 @@ const registerUser = asyncHandler( async(req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath; // dono hi same tarika hai file path save karne ka 
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if( !avatarLocalPath ){
         throw new ApiError(400, "Avatar file is required")
     }
